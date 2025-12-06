ec3f02e54a756ea7c5f519782a1ebd9914be9a6d
Yug Dandawala <yugdandawala3@gmail.com>
2025-12-06 13:00:31 +0530

    Update huggingdownload.py

--- EmbeddedModels/huggingdownload.py
+++ EmbeddedModels/huggingdownload.py
@@ -1,3 +1,5 @@
+# This is for downlaoding the emebdding models only not the WHole LLM model
+
 from langchain_huggingface import HuggingFaceEmbeddings
 
 embeddings=HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
