e43fb2b5a81bd471cc8b1e552ac05a9a3038cc19
Yug Dandawala <yugdandawala3@gmail.com>
2026-01-01 17:21:00 +0530

    Update CurrentMethodAgent.py

--- Agents/CurrentMethodAgent.py
+++ Agents/CurrentMethodAgent.py
@@ -13,7 +13,7 @@ def get_weather_data(city: str) -> str:
     """
     This function fetches the current weather data for a given city
     """
-    url = f"https://api.weatherstack.com/current?access_key=4d1d8ae207a8c845a52df8a67bf3623e&query={city}"
+    url = f"https://api.weatherstack.com/current?access_key=api_key&query={city}"
     response = requests.get(url)
     return response.json
 
